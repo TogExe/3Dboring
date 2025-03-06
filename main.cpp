760f8bab423de5efe5f329c11642f4d765fb83f8
TogExe <66035138+TogExe@users.noreply.github.com>
2025-03-06 09:25:23 +0100

    Update main.cpp
    changing the main to support generation and for testing

--- main.cpp
+++ main.cpp
@@ -4,14 +4,17 @@
 #include <vector>
 #include <chrono>
 #include <stdio.h>
+#include <execution>
+#include <algorithm>
+#include "donjon.h"
 
 #define TRUE 1
 #define FALSE 0
 #define DEBUG TRUE
-#define HEIGHT 400
-#define WIDTH 400
-#define FOV 90
-#define PI 3.14159265
+#define HEIGHT 720
+#define WIDTH 720
+#define FOV 45
+constexpr float PI = 3.14159265f;
 
 int update = true;
 
@@ -22,68 +25,54 @@ float player_z_offset = 7.0f;
 float camera_offset_x = 0.0f;
 float camera_offset_y = 0.0f;
 float camera_offset_z = 0.0f;
-float camera_rotation[3] = {0., 0., 0.};
+float camera_rotation[3] = {0.f, 0.f, 0.f};
 
-typedef struct Point {
+struct Point {
     float pos_x, pos_y, pos_z;
-} Point;
+};
 
-typedef struct Vertex {
+struct Vertex {
     Point point;
-    struct Vertex* next;
+    Vertex* next;
     enum { HEAD, BASIC } type;
-} Vertex;
+};
 
-typedef struct Triangle {
+struct Triangle {
     Vertex vertex[3];
-} Triangle;
+    Point*dvec;
+};
 
-typedef struct Environment {
-    int size;
-    Triangle* triangles;
-} Environment;
-
-typedef struct Model {
+struct Environment {
     std::vector<Triangle> triangles;
-} Model;
+};
 
-typedef struct Renderable {
+struct Model {
+    std::vector<Triangle> triangles;
+};
+
+struct Renderable {
     Model* model;
     float offsets[3]; // x, y, z position
     int rotations[3]; // rotations around x, y, z
-    struct Renderable* next;
-} Renderable;
+    Renderable* next;
+};
 
-Renderable* head = NULL;
+Renderable* head = nullptr;
 
 void setupEnvironment(Environment* env, int size_) {
-    env->size = size_;
-    env->triangles = (Triangle*)malloc(size_ * sizeof(Triangle));
-    if (env->triangles == NULL) {
-        fprintf(stderr, "Erreur d'allocation de mémoire\n");
-        exit(1);
-    }
+    env->triangles.reserve(size_);
 }
 
 void addTriangle(Environment* env, Vertex* v1, Vertex* v2, Vertex* v3) {
-    env->size += 1;
-    env->triangles = (Triangle*)realloc(env->triangles, env->size * sizeof(Triangle));
-    if (env->triangles == NULL) {
-        fprintf(stderr, "Erreur de réallocation de mémoire\n");
-        exit(1);
-    }
-    env->triangles[env->size - 1] = (Triangle){ *v1, *v2, *v3 };
+    env->triangles.emplace_back(Triangle{ *v1, *v2, *v3 });
 }
 
 Triangle makeTriangle(Point point[3]) {
     Vertex vertex[3];
     Triangle triangle;
 
-    vertex[0].point = point[0];
-    vertex[1].point = point[1];
-    vertex[2].point = point[2];
-
     for (int i = 0; i < 3; i++) {
+        vertex[i].point = point[i];
         vertex[i].next = &vertex[(i + 1) % 3];
         triangle.vertex[i] = vertex[i];
     }
@@ -91,10 +80,9 @@ Triangle makeTriangle(Point point[3]) {
 }
 
 void forwardMove(float d) {
-    float pi = PI;
-    float forward_z = d * cos(pi * camera_rotation[1]) * cos(pi * camera_rotation[0]);
-    float forward_y = d * sin(pi * camera_rotation[0]);
-    float forward_x = d * sin(pi * camera_rotation[1]) * cos(pi * camera_rotation[0]);
+    float forward_z = d * cos(PI * camera_rotation[1]) * cos(PI * camera_rotation[0]);
+    float forward_y = d * sin(PI * camera_rotation[0]);
+    float forward_x = d * sin(PI * camera_rotation[1]) * cos(PI * camera_rotation[0]);
 
     camera_offset_x += forward_x;
     camera_offset_y += forward_y;
@@ -105,12 +93,8 @@ void DrawTriangle(HDC hdc, Triangle* triangle) {
     Gdiplus::Graphics graphics(hdc);
     Gdiplus::Pen pen(Gdiplus::Color(255, 0, 0, 255));
     for (int i = 0; i < 3; i++) {
-        Gdiplus::PointF pt1(
-            triangle->vertex[i].point.pos_x,
-            triangle->vertex[i].point.pos_y);
-        Gdiplus::PointF pt2(
-            triangle->vertex[(i + 1) % 3].point.pos_x,
-            triangle->vertex[(i + 1) % 3].point.pos_y);
+        Gdiplus::PointF pt1(triangle->vertex[i].point.pos_x, triangle->vertex[i].point.pos_y);
+        Gdiplus::PointF pt2(triangle->vertex[(i + 1) % 3].point.pos_x, triangle->vertex[(i + 1) % 3].point.pos_y);
         graphics.DrawLine(&pen, pt1, pt2);
     }
 }
@@ -118,7 +102,7 @@ void DrawTriangle(HDC hdc, Triangle* triangle) {
 float ProjectionMatrix[4][4] = {0};
 
 void setProjectionMatrix(const float& angleOfView, const float& near_, const float& far_) {
-    float scale = 1 / tan(angleOfView * 0.5 * M_PI / 180);
+    float scale = 1 / tan(angleOfView * 0.5f * PI / 180.0f);
     ProjectionMatrix[0][0] = scale;
     ProjectionMatrix[1][1] = scale;
     ProjectionMatrix[2][2] = -far_ / (far_ - near_);
@@ -127,17 +111,17 @@ void setProjectionMatrix(const float& angleOfView, const float& near_, const flo
     ProjectionMatrix[3][3] = 0;
     if (DEBUG) {
         for (int i = 0; i < 4; i++) {
-            printf("\n");
             for (int j = 0; j < 4; j++) {
                 printf("%.2f ", ProjectionMatrix[i][j]);
             }
+            printf("\n");
         }
     }
 }
 
-typedef struct ScreenPoint {
+struct ScreenPoint {
     float x, y;
-} ScreenPoint;
+};
 
 ScreenPoint ProjectPoint(const Point& point) {
     float x = point.pos_x * ProjectionMatrix[0][0];
@@ -154,22 +138,43 @@ ScreenPoint ProjectPoint(const Point& point) {
 }
 
 Point rotatePoint(const Point& point) {
-    float pi = PI;
     Point rotatedPoint = point;
     float rp[3] = {
         point.pos_x + camera_offset_x,
-        (point.pos_y + camera_offset_y) * cos(pi * camera_rotation[0]) - (point.pos_z + camera_offset_z) * sin(pi * camera_rotation[0]),
-        (point.pos_y + camera_offset_y) * sin(camera_rotation[0] * pi) + (point.pos_z + camera_offset_z) * cos(camera_rotation[0] * pi)
+        (point.pos_y + camera_offset_y) * cos(PI * camera_rotation[0]) - (point.pos_z + camera_offset_z) * sin(PI * camera_rotation[0]),
+        (point.pos_y + camera_offset_y) * sin(PI * camera_rotation[0]) + (point.pos_z + camera_offset_z) * cos(PI * camera_rotation[0])
     };
     float rp_[3] = {
-        rp[0] * cos(camera_rotation[2] * pi) - rp[1] * sin(camera_rotation[2] * pi),
-        rp[0] * sin(camera_rotation[2] * pi) + rp[1] * cos(camera_rotation[2] * pi),
+        rp[0] * cos(PI * camera_rotation[2]) - rp[1] * sin(PI * camera_rotation[2]),
+        rp[0] * sin(PI * camera_rotation[2]) + rp[1] * cos(PI * camera_rotation[2]),
         rp[2],
     };
     float rp__[3] = {
-        rp_[0] * cos(camera_rotation[1] * pi) - rp_[2] * sin(camera_rotation[1] * pi),
+        rp_[0] * cos(PI * camera_rotation[1]) - rp_[2] * sin(PI * camera_rotation[1]),
         rp_[1],
-        rp_[0] * sin(camera_rotation[1] * pi) + rp_[2] * cos(camera_rotation[1] * pi)
+        rp_[0] * sin(PI * camera_rotation[1]) + rp_[2] * cos(PI * camera_rotation[1])
+    };
+    rotatedPoint.pos_x = rp__[0];
+    rotatedPoint.pos_y = rp__[1];
+    rotatedPoint.pos_z = rp__[2];
+    return rotatedPoint;
+}
+Point rotateVec(const Point& point) {
+    Point rotatedPoint = point;
+    float rp[3] = {
+        point.pos_x,
+        (point.pos_y) * cos(PI * camera_rotation[0]) - (point.pos_z) * sin(PI * camera_rotation[0]),
+        (point.pos_y) * sin(PI * camera_rotation[0]) + (point.pos_z) * cos(PI * camera_rotation[0])
+    };
+    float rp_[3] = {
+        rp[0] * cos(PI * camera_rotation[2]) - rp[1] * sin(PI * camera_rotation[2]),
+        rp[0] * sin(PI * camera_rotation[2]) + rp[1] * cos(PI * camera_rotation[2]),
+        rp[2],
+    };
+    float rp__[3] = {
+        rp_[0] * cos(PI * camera_rotation[1]) - rp_[2] * sin(PI * camera_rotation[1]),
+        rp_[1],
+        rp_[0] * sin(PI * camera_rotation[1]) + rp_[2] * cos(PI * camera_rotation[1])
     };
     rotatedPoint.pos_x = rp__[0];
     rotatedPoint.pos_y = rp__[1];
@@ -178,22 +183,21 @@ Point rotatePoint(const Point& point) {
 }
 
 Point rotatePointAO(const Point& point, float* off) {
-    float pi = PI;
     Point rotatedPoint = point;
     float rp[3] = {
         point.pos_x + camera_offset_x + off[0],
-        (point.pos_y + camera_offset_y + off[1]) * cos(pi * camera_rotation[0]) - (point.pos_z + camera_offset_z + off[2]) * sin(pi * camera_rotation[0]),
-        (point.pos_y + camera_offset_y + off[1]) * sin(camera_rotation[0] * pi) + (point.pos_z + camera_offset_z + off[2]) * cos(camera_rotation[0] * pi)
+        (point.pos_y + camera_offset_y + off[1]) * cos(PI * camera_rotation[0]) - (point.pos_z + camera_offset_z - off[2]) * sin(PI * camera_rotation[0]),
+        (point.pos_y + camera_offset_y + off[1]) * sin(PI * camera_rotation[0]) + (point.pos_z + camera_offset_z - off[2]) * cos(PI * camera_rotation[0])
     };
     float rp_[3] = {
-        rp[0] * cos(camera_rotation[2] * pi) - rp[1] * sin(camera_rotation[2] * pi),
-        rp[0] * sin(camera_rotation[2] * pi) + rp[1] * cos(camera_rotation[2] * pi),
+        rp[0] * cos(PI * camera_rotation[2]) - rp[1] * sin(PI * camera_rotation[2]),
+        rp[0] * sin(PI * camera_rotation[2]) + rp[1] * cos(PI * camera_rotation[2]),
         rp[2],
     };
     float rp__[3] = {
-        rp_[0] * cos(camera_rotation[1] * pi) - rp_[2] * sin(camera_rotation[1] * pi),
+        rp_[0] * cos(PI * camera_rotation[1]) - rp_[2] * sin(PI * camera_rotation[1]),
         rp_[1],
-        rp_[0] * sin(camera_rotation[1] * pi) + rp_[2] * cos(camera_rotation[1] * pi)
+        rp_[0] * sin(PI * camera_rotation[1]) + rp_[2] * cos(PI * camera_rotation[1])
     };
     rotatedPoint.pos_x = rp__[0];
     rotatedPoint.pos_y = rp__[1];
@@ -202,9 +206,22 @@ Point rotatePointAO(const Point& point, float* off) {
 }
 
 bool IsTriangleVisible(const Triangle& triangle) {
-    return rotatePoint(triangle.vertex[0].point).pos_z <= 0 ||
-           rotatePoint(triangle.vertex[1].point).pos_z <= 0 ||
-           rotatePoint(triangle.vertex[2].point).pos_z <= 0;
+    return
+        rotatePoint(triangle.vertex[0].point).pos_z <= 0 ||
+            rotatePoint(triangle.vertex[1].point).pos_z <= 0 ||
+                rotatePoint(triangle.vertex[2].point).pos_z <= 0;//rotateVec(*triangle.dvec).pos_z <=0;
+}
+
+bool IsTriangleVisibleWithOffset(const Triangle& triangle, float* off,Point * a) {
+    a[0] = rotatePointAO(triangle.vertex[0].point, off);
+    a[1] = rotatePointAO(triangle.vertex[1].point, off);
+    a[2] = rotatePointAO(triangle.vertex[2].point, off);
+
+    if (a[0].pos_z <= 0 ||
+           a[1].pos_z <= 0 ||
+           a[2].pos_z <= 0) {
+        return false;
+    }else{return true;}
 }
 
 void DrawTriangle3D(HDC hdc, Triangle* triangle) {
@@ -231,17 +248,15 @@ void DrawTriangle3D(HDC hdc, Triangle* triangle) {
 }
 
 void DrawTriangle3DWithOffset(HDC hdc, Triangle* triangle, float* off) {
-    if (IsTriangleVisible(*triangle)) {
-        return;
-    }
-
+    Point a[3];
+    if (IsTriangleVisibleWithOffset(*triangle,off,a) == false) {return;}
     Gdiplus::Graphics graphics(hdc);
-    Gdiplus::Pen pen(Gdiplus::Color(100, 70, 120, 174));
-
+    Gdiplus::Pen pen(Gdiplus::Color(255, 50, 100, 100));
+    Gdiplus::SolidBrush brush(Gdiplus::Color(200, 40, 90, 120));
     ScreenPoint points_s[3] = {
-        ProjectPoint(rotatePointAO(triangle->vertex[0].point, off)),
-        ProjectPoint(rotatePointAO(triangle->vertex[1].point, off)),
-        ProjectPoint(rotatePointAO(triangle->vertex[2].point, off))
+        ProjectPoint(a[0]),
+        ProjectPoint(a[1]),
+        ProjectPoint(a[2])
     };
 
     Gdiplus::PointF points[] = {
@@ -249,7 +264,7 @@ void DrawTriangle3DWithOffset(HDC hdc, Triangle* triangle, float* off) {
         Gdiplus::PointF(points_s[1].x, points_s[1].y),
         Gdiplus::PointF(points_s[2].x, points_s[2].y)
     };
-
+    //graphics.FillPolygon(&brush,points,3);
     graphics.DrawLines(&pen, points, 3);
 }
 
@@ -282,21 +297,21 @@ Model makeCube(float size, float x_offset, float y_offset, float z_offset) {
             vertices[indices[i][1]],
             vertices[indices[i][2]]
         };
-        cube.triangles.push_back(makeTriangle(triangle_vertices));
+        cube.triangles.emplace_back(makeTriangle(triangle_vertices));
     }
 
     return cube;
 }
 
 void DrawModel3D(HDC hdc, Model* model) {
-    for (Triangle& triangle : model->triangles) {
-        DrawTriangle3D(hdc, &triangle);
+    for (const Triangle& triangle : model->triangles) {
+        DrawTriangle3D(hdc, const_cast<Triangle*>(&triangle));
     }
 }
 
 void DrawModelWithOffsetIn3d(HDC hdc, Model* model, float* off) {
-    for (Triangle& triangle : model->triangles) {
-        DrawTriangle3DWithOffset(hdc, &triangle, off);
+    for (const Triangle& triangle : model->triangles) {
+        DrawTriangle3DWithOffset(hdc, const_cast<Triangle*>(&triangle), off);
     }
 }
 
@@ -305,23 +320,36 @@ void drawRenderable(HDC hdc, Renderable* renderable) {
 }
 
 void addRenderable(Renderable** head, Renderable* newRenderable) {
-    if (*head == NULL) {
+    if (*head == nullptr) {
         *head = newRenderable;
     } else {
         Renderable* current = *head;
-        while (current->next != NULL) {
+        while (current->next != nullptr) {
             current = current->next;
         }
         current->next = newRenderable;
     }
-    newRenderable->next = NULL;
+    newRenderable->next = nullptr;
 }
 
 void drawRenderables(HDC hdc, Renderable** head) {
+    std::vector<Renderable*> renderables;
     Renderable* current = *head;
-    while (current != NULL) {
-        drawRenderable(hdc, current);
+    while (current != nullptr) {
+        renderables.push_back(current);
         current = current->next;
+    }
+
+    // Trier les objets par distance par rapport à la caméra
+    std::sort(renderables.begin(), renderables.end(), [](Renderable* a, Renderable* b) {
+        float distA = sqrt(a->offsets[0] * a->offsets[0] + a->offsets[1] * a->offsets[1] + a->offsets[2] * a->offsets[2]);
+        float distB = sqrt(b->offsets[0] * b->offsets[0] + b->offsets[1] * b->offsets[1] + b->offsets[2] * b->offsets[2]);
+        return distA > distB; // Dessiner du plus éloigné au plus proche
+    });
+
+    // Dessiner les objets triés
+    for (Renderable* renderable : renderables) {
+        drawRenderable(hdc, renderable);
     }
 }
 
@@ -350,7 +378,7 @@ Model createPyramid(float size, float x_offset, float y_offset, float z_offset) 
             vertices[indices[_][1]],
             vertices[indices[_][2]]
         };
-        pyramid.triangles.push_back(makeTriangle(triangle_vertices));
+        pyramid.triangles.emplace_back(makeTriangle(triangle_vertices));
     }
 
     return pyramid;
@@ -365,9 +393,9 @@ void HandleInput(WPARAM wParam, bool isPressed) {
 }
 
 bool helpSpaceKey = true;
-Model cube = makeCube(1.0f, 0, 0, 0);
-Model pyr = createPyramid(2.0f, player_x_offset + 10, player_y_offset - 3, player_z_offset + 1);
-
+Model cube = makeCube(0.5f, 0, 0, 0);
+Model pyr = createPyramid(0.5f, player_x_offset + 10, player_y_offset - 3, player_z_offset + 1);
+Point facing ={camera_rotation[0], camera_rotation[1], camera_rotation[2]};
 void UpdateMovement() {
     if (keys[VK_UP]) {
         forwardMove(-0.1f);
@@ -394,20 +422,22 @@ void UpdateMovement() {
         update = true;
     }
     if (keys['S']) {
-        camera_rotation[0] -= 0.05f;
+        camera_rotation[2] +=0.03*cosf(camera_rotation[1]);
+        camera_rotation[0] += 0.03*sinf(camera_rotation[1]);
         update = true;
     }
     if (keys['Z']) {
-        camera_rotation[0] += 0.05f;
+        camera_rotation[2] -=0.03*cosf(camera_rotation[1]);
+        camera_rotation[0] -= 0.03*sinf(camera_rotation[1]);
         update = true;
     }
     if (keys[VK_SPACE]) {
         if (helpSpaceKey) {
-            Renderable* newRenderable = (Renderable*)malloc(sizeof(Renderable));
+            Renderable* newRenderable = new Renderable();
             newRenderable->model = &cube;
             newRenderable->offsets[0] = -camera_offset_x; // Set x offset
-            newRenderable->offsets[1] = camera_offset_y; // Set y offset
-            newRenderable->offsets[2] = -camera_offset_z; // Set z offset
+            newRenderable->offsets[1] = -camera_offset_y; // Set y offset
+            newRenderable->offsets[2] = camera_offset_z; // Set z offset
             newRenderable->rotations[0] = 0;  // Set x rotation
             newRenderable->rotations[1] = 0;  // Set y rotation
             newRenderable->rotations[2] = 0;  // Set z rotation
@@ -446,13 +476,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         case WM_KEYDOWN:
             HandleInput(wParam, true);
             if (update) {
-                InvalidateRect(hwnd, NULL, TRUE);
+                InvalidateRect(hwnd, nullptr, TRUE);
             }
             break;
         case WM_KEYUP:
             HandleInput(wParam, false);
             if (update) {
-                InvalidateRect(hwnd, NULL, TRUE);
+                InvalidateRect(hwnd, nullptr, TRUE);
             }
             break;
         case WM_PAINT: {
@@ -471,28 +501,56 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
     return 0;
 }
 
+void freeRenderables(Renderable** head) {
+    Renderable* current = *head;
+    while (current != nullptr) {
+        Renderable* next = current->next;
+        delete current;
+        current = next;
+    }
+    *head = nullptr;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    setProjectionMatrix(FOV, 0.5f, 1000.0f);
+    setProjectionMatrix(FOV, 0.01f, 1000.0f);
     Gdiplus::GdiplusStartupInput gdiplusStartupInput;
     ULONG_PTR gdiplusToken;
-    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
+    Gdiplus::GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, nullptr);
+    static char donjon[DONJON_HAUTEUR][DONJON_LARGEUR] = {0};
+    generer_donjon(donjon);
+
+    for (int y = 0; y < DONJON_HAUTEUR; y++) {
+        for (int x = 0; x < DONJON_LARGEUR; x++) {
+            if (donjon[y][x] == 1) {
+                Renderable* newRenderable = new Renderable();
+                newRenderable->model = &cube;
+                newRenderable->offsets[0] = static_cast<float>(y) - 50; // Set x offset
+                newRenderable->offsets[1] = 0; // Set y offset
+                newRenderable->offsets[2] = static_cast<float>(x) - 50; // Set z offset
+                newRenderable->rotations[0] = 0;  // Set x rotation
+                newRenderable->rotations[1] = 0;  // Set y rotation
+                newRenderable->rotations[2] = 0;  // Set z rotation
+                addRenderable(&head, newRenderable);
+            }
+        }
+    }
 
     WNDCLASSEX wc = { sizeof(WNDCLASSEX), CS_CLASSDC, WindowProc, 0L, 0L,
-                      GetModuleHandle(NULL), NULL, NULL, NULL, NULL,
-                      "GDI+ Player Model Window", NULL };
+                      GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr,
+                      "GDI+ Player Model Window", nullptr };
     RegisterClassEx(&wc);
     HWND hwnd = CreateWindow(wc.lpszClassName, "Flight Simulator 2001",
                              WS_OVERLAPPEDWINDOW, 0, 0, WIDTH, HEIGHT,
-                             NULL, NULL, wc.hInstance, NULL);
+                             nullptr, nullptr, wc.hInstance, nullptr);
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
     MSG msg;
     auto lastFrameTime = std::chrono::high_resolution_clock::now();
-    const double targetFrameTime = 1.0 / 120.0;
+    const double targetFrameTime = 1.0 / 30.0;
 
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
         auto now = std::chrono::high_resolution_clock::now();
@@ -504,6 +562,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         MainLoop();
     }
 
+    freeRenderables(&head);
     Gdiplus::GdiplusShutdown(gdiplusToken);
     UnregisterClass(wc.lpszClassName, wc.hInstance);
     return (int)msg.wParam;
